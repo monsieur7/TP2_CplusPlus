e9572ab59639a6b61bad1c7ec59437ef75c1e7eb
monsieur7 <nolane.de@gmail.com>
2022-12-07 13:59:01 +0100

    3 rotors !

--- Enigma.cpp
+++ Enigma.cpp
@@ -6,6 +6,10 @@ void Enigma::Encode(){
             if(c == '\n'){
                 _cipher.push_back('\n');
             }
+            else if(c == ' '){
+            _cipher.push_back(' ');
+
+            }
             else {
                 char new_char;
                 int local_offset;
@@ -27,11 +31,17 @@ void Enigma::Encode(){
                 for(int i =_numRotor-1; i >=0; i--){
                     
                     if(i == _numRotor - 1){
-                      offset_reverse = offsetReverse(reflected, _key.at(i));
+                      offset_reverse = offsetReverse(reflected, _key.at(i),0);
                     }
                     else {
-                     offset_reverse = offsetReverse(reverse, _key.at(i));
+                     offset_reverse = offsetReverse(reverse, _key.at(i), 0);
 
+                    }
+                    if(offset_reverse - _offset.at(i) < 0){ // because if we want to offset A => it need to be a Z
+                        offset_reverse = 26 + (offset_reverse - _offset.at(i));
+                    }
+                    else {
+                        offset_reverse = (offset_reverse - _offset.at(i));
                     }
                     reverse = (char)(65+offset_reverse); // reverse connexion
                 }
@@ -51,15 +61,71 @@ void Enigma::Encode(){
             
 }
 void Enigma::Decode(){
-    //TODO
-}   
-Enigma::Enigma(std::string rotor1, std::string rotor2){
-    if(rotor1.length() != 26 || rotor2.length() != 26){
+_plain = "";
+    for(char c : _cipher){
+            if(c == '\n'){
+                _plain.push_back('\n');
+            }
+            else if(c == ' '){
+            _plain.push_back(' ');
+
+            }
+            else {
+                char new_char;
+                int local_offset;
+                for(int i = 0; i < _numRotor; i ++){
+                if(i == 0){
+                    local_offset = (c - 65 + _offset.at(i)) % 26; // everything in MAJ
+                }
+                else {
+                    local_offset = (new_char - 65 + _offset.at(i)) % 26; // everything in MAJ
+
+                }
+                    new_char = _key.at(i)[local_offset];
+                }
+                char reflected = _reflector.at((int)new_char - 65);
+
+                int offset_reverse;
+                char reverse;
+
+                for(int i =_numRotor-1; i >=0; i--){
+                    
+                    if(i == _numRotor - 1){
+                      offset_reverse = offsetReverse(reflected, _key.at(i),0);
+                    }
+                    else {
+                     offset_reverse = offsetReverse(reverse, _key.at(i), 0);
+
+                    }
+                    if(offset_reverse - _offset.at(i) < 0){ // because if we want to offset A => it need to be a Z
+                        offset_reverse = 26 + (offset_reverse - _offset.at(i));
+                    }
+                    else {
+                        offset_reverse = (offset_reverse - _offset.at(i));
+                    }
+                    reverse = (char)(65+offset_reverse); // reverse connexion
+                }
+                
+
+                _plain.push_back(reverse);
+                _offset.at(0) = _offset.at(0) + 1;
+
+                for(int i = 1; i < _numRotor; i++){
+                    if(_offset.at(i-1) == 27){
+                        _offset.at(i-1) = 0;
+                        _offset.at(i) += 1;
+                    }
+                }
+            }
+        }}   
+Enigma::Enigma(std::string rotor1, std::string rotor2,  std::string rotor3){
+    if(rotor1.length() != 26 || rotor2.length() != 26 ||  rotor3.length() != 26 ){
         throw std::runtime_error("not the right key lenght");
     }
     else {
         _key.push_back(rotor1);
         _key.push_back(rotor2);
+        _key.push_back(rotor3);
 
     }
     _offset.resize(_numRotor); // one rotor
@@ -69,18 +135,20 @@ Enigma::Enigma(std::string rotor1, std::string rotor2){
 }
 
 
-std::string Enigma::getCipher(){
+std::string& Enigma::getCipher(){
     return _cipher;
 } // FOR DEBUG
-
+std::string& Enigma::getPlain(){
+    return _plain;
+}
 void Enigma::setPlain(std::string plain){
     _plain = plain;
 }
 
-int Enigma::offsetReverse(char c, std::string key){
+int Enigma::offsetReverse(char c, std::string key, int offset){
     for(int i = 0; i < 26; i++){
-        if(key.at(i) == c){
-            return i;
+        if(key.at(i+offset) == c){ // i + offset because we need to take into account the fact that the rotor may have rotated (by offset), so the output letter that we are searching for is not the same
+            return (i);
         }
     }
     return 0; // ERROR TODO
--- Enigma.h
+++ Enigma.h
@@ -6,14 +6,16 @@ class Enigma : public Encrypt {
     private:
         std::vector<std::string> _key; // one rotor but a vector for later
         std::vector<int> _offset;
-        std::string _reflector = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
-        int offsetReverse(char c, std::string key);
-        int _numRotor = 2;
+        //std::string _reflector = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+        std::string _reflector = "YRUHQSLDPXNGOKMIEBFZCWVJAT"; // B REFLECTOR
+        int offsetReverse(char c, std::string key, int offset);
+        int _numRotor = 3;
     public:
-        Enigma(std::string rotor1, std::string rotor2); // only one rotor
+        Enigma(std::string rotor1, std::string rotor2,  std::string rotor3); // only one rotor
         void Encode();
         void Decode();
-        std::string getCipher(); // FOR DEBUG
+        std::string& getCipher(); // FOR DEBUG
+        std::string& getPlain();
         void setPlain(std::string plain);
         void reset(); // reset offset at 0
         void setOffset(std::vector<int> offset);

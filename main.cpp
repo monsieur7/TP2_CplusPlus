b3eb5244fff99d25aecc65ef6818cddf66f0495a
monsieur7 <nolane.de@gmail.com>
2022-12-08 13:57:25 +0100

    fix problems

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char ** argv){
     Enigma e1("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "AJDKSIRUXBLHWTMCQGZNPYFVOE", "BDFHJLCPRTXVZNYEIWGAKMUSQO"); // THREE ROTORS ENIGMA => https://en.wikipedia.org/wiki/Enigma_rotor_details
     // B REFLECTOR IS USED
     // THE MACHINE IS SYMETRIC SO DECODE AND ENCODE DO THE SAME THING EXCEPTED THAT encode goes from plain to text and decode does the reverse
-    e1.read("../test.txt")
+    e1.read("../test.txt");
     e1.Encode();
     std::cout << "result " << e1.getCipher() << std::endl;
     e1.reset(); // RESET STATE (rotors offset)

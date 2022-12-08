5909c9660bf2a584019645349a4657e06e851bc1
monsieur7 <nolane.de@gmail.com>
2022-12-08 13:57:01 +0100

    main test

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@ int main(int argc, char ** argv){
     Enigma e1("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "AJDKSIRUXBLHWTMCQGZNPYFVOE", "BDFHJLCPRTXVZNYEIWGAKMUSQO"); // THREE ROTORS ENIGMA => https://en.wikipedia.org/wiki/Enigma_rotor_details
     // B REFLECTOR IS USED
     // THE MACHINE IS SYMETRIC SO DECODE AND ENCODE DO THE SAME THING EXCEPTED THAT encode goes from plain to text and decode does the reverse
-    e1.setPlain("WETTERBERICHT\n WEATHER IS GOOD\n"); // :) 
+    e1.read("../test.txt")
     e1.Encode();
     std::cout << "result " << e1.getCipher() << std::endl;
     e1.reset(); // RESET STATE (rotors offset)
     e1.Decode();
     std::cout << "decoded " << e1.getPlain();
+    
     return 0;
 }
